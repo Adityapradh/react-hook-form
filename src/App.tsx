5be40a86d7d56853b89a49809d2b4775533182a7
Aditya pradhan <pradhanadi741@gmail.com>
2025-07-19 23:43:32 +0530

    feat: add readonly blur test logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import AccessibleForm from './Accessibleform'; // ✅ if file is Accessibleform.tsx
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>React Hook Form - Accessible Example</h2>
+      <AccessibleForm />  {/* ✅ ये component render किया */}
     </div>
   );
 }
+
+
+
+
 
 export default App;
